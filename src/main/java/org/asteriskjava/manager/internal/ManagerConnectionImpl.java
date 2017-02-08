bbd114e10c856b0ba5649ff8ac293a18943e991e
Sean Bright <sean.bright@gmail.com>
2017-02-08 13:29:21 +0100

    Asterisk 14 version support
    (cherry picked from commit 5ba29c78526d8b6ce394604ac6f060d9c5d71015)

--- src/main/java/org/asteriskjava/manager/internal/ManagerConnectionImpl.java
+++ src/main/java/org/asteriskjava/manager/internal/ManagerConnectionImpl.java
@@ -95,6 +95,7 @@ public class ManagerConnectionImpl implements ManagerConnection, Dispatcher
     private static final Pattern VERSION_PATTERN_11 = Pattern.compile("^\\s*Asterisk ((SVN-branch|GIT)-)?11[-. ].*");
     private static final Pattern VERSION_PATTERN_12 = Pattern.compile("^\\s*Asterisk ((SVN-branch|GIT)-)?12[-. ].*");
     private static final Pattern VERSION_PATTERN_13 = Pattern.compile("^\\s*Asterisk ((SVN-branch|GIT)-)?13[-. ].*");
+    private static final Pattern VERSION_PATTERN_14 = Pattern.compile("^\\s*Asterisk (GIT-)?14[-. ].*");
 
     private static final AtomicLong idCounter = new AtomicLong(0);
 
@@ -730,6 +731,10 @@ public class ManagerConnectionImpl implements ManagerConnection, Dispatcher
                             else if (VERSION_PATTERN_13.matcher(coreLine).matches())
                             {
                                 return AsteriskVersion.ASTERISK_13;
+                            }
+                            else if (VERSION_PATTERN_14.matcher(coreLine).matches())
+                            {
+                                return AsteriskVersion.ASTERISK_14;
                             }
                         }
                     }
