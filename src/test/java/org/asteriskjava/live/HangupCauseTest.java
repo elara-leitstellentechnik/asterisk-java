c0b2b1b4e07d9c00e75db82ea9854651fe9f5c1d
sere <github@elara-gmbh.de>
2021-04-22 11:07:25 +0200

    Renaming in HangupCause

--- src/test/java/org/asteriskjava/live/HangupCauseTest.java
+++ src/test/java/org/asteriskjava/live/HangupCauseTest.java
@@ -9,6 +9,12 @@ public class HangupCauseTest
     @Test
     public void testGetByCode()
     {
-        assertEquals("Valid enum for cause code 18", HangupCause.AST_CAUSE_NO_USER_RESPONSE, HangupCause.getByCode(18));
+        assertEquals("Valid enum for cause code 18", HangupCause.AST_CAUSE_18_NO_USER_RESPONSE, HangupCause.getByCode(18));
+    }
+
+    @Test
+    public void testToString()
+    {
+        assertEquals("Valid toString for cause code 18", "NO_USER_RESPONSE", HangupCause.AST_CAUSE_18_NO_USER_RESPONSE.toString());
     }
 }
